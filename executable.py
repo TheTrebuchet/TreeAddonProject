6b97251757e7f2d1a2a51b8062333c17d94c3e02
TheTrebuchet <jan.kulczycki1@gmail.com>
2022-09-09 23:07:17 +0200

    basic file

---
+++ executable.py
@@ -0,0 +1,13 @@
+import bpy
+
+
+
+
+verts =
+
+mesh = bpy.data.meshes.new("tree")
+object = bpy.data.objects.new("tree", mesh)
+
+bpy.context.collection.objects.link(object)
+
+mesh.from_pydata(verts,[], faces)
