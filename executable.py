ef6c2a37aaed510da09e5f8e194feb944a033453
TheTrebuchet <jan.kulczycki1@gmail.com>
2022-09-09 23:59:05 +0200

    added blend file, made a function for creating circles, added way for executable to import * from functions

--- executable.py
+++ executable.py
@@ -1,11 +1,25 @@
 import bpy
 import math
-from functions.py import *
+import sys
+import os
 
+dir = os.path.dirname(bpy.data.filepath)
+if not dir in sys.path:
+    sys.path.append(dir )
 
+import functions
 
+# this next part forces a reload in case you edit the source after you first start the blender session
+import imp
+imp.reload(functions)
 
-verts =
+# this is optional and allows you to call the functions without specifying the package name
+from functions import *
+
+n = 50
+r=1
+verts = circle(n,r)
+faces = [tuple(x for x in range(n))]
 
 mesh = bpy.data.meshes.new("tree")
 object = bpy.data.objects.new("tree", mesh)
--- functions.py
+++ functions.py
@@ -1,7 +1,13 @@
+import math
+
 def circle(n,r):
     circle = []
     if n<3 or r==0:
         return []
     else:
-        for i in range(n+1):
-            circle.append((r*math.cos(2*math.pi*i/n), r*math.sin(2*math.pi*i/n))0)
+        for i in range(n):
+            circle.append((r*math.cos(2*math.pi*i/n), r*math.sin(2*math.pi*i/n), 0))
+    return circle
+
+if __name__ == "__main__":
+    print(circle(4,1))
