7c771726326dd3444c25013068d2a75a5ba11241
TheTrebuchet <jan.kulczycki1@gmail.com>
2022-09-12 08:39:11 +0200

    succesfully added spine function

--- executable.py
+++ executable.py
@@ -15,15 +15,23 @@ imp.reload(functions)
 
 # this is optional and allows you to call the functions without specifying the package name
 from functions import *
-
-n = 50
-r=1
-verts = circle(n,r)
-faces = [tuple(x for x in range(n))]
+# MAIN PARAMETERS
+n = 5
+res = 1
+scale = 0.5
+# ADDITIONAL PARAMETERS
+s=6*res
+r=res
+l=r*2
+tree = []
+for x in spine(n, l):
+    for y in circle(s,r):
+        tree.append((mathutils.Vector(x) + mathutils.Vector(y))*scale)
+faces = bark(tree, s, n)
 
 mesh = bpy.data.meshes.new("tree")
 object = bpy.data.objects.new("tree", mesh)
 
 bpy.context.collection.objects.link(object)
 
-mesh.from_pydata(verts,[], faces)
+mesh.from_pydata(tree,[], faces)
--- functions.py
+++ functions.py
@@ -1,5 +1,7 @@
 import math
+import mathutils
 
+# number of sides, radius
 def circle(n,r):
     circle = []
     if n<3 or r==0:
@@ -9,5 +11,27 @@ def circle(n,r):
             circle.append((r*math.cos(2*math.pi*i/n), r*math.sin(2*math.pi*i/n), 0))
     return circle
 
+# number of vertices, length
+def spine(n, l):
+    spine = [(0, 0, l*i) for i in range(n)]
+    return spine
+# vertices, number of sides, number of circles
+def bark(verts, s, n):
+    faces =[]
+    for x in range(0,n):
+        for x in range(s):
+                faces.append(tuple())
+    return faces
+
+
+
 if __name__ == "__main__":
-    print(circle(4,1))
+#place for testing
+    n = 4
+    r = 1
+    l = 1
+    tree = []
+    for x in spine(n, l):
+        for y in circle(n,r):
+            tree.append(tuple(mathutils.vector(x) + mathutils.vector(y)))
+    print(tree)
